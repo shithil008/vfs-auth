8af72879907100eff86972b09c2a3a4f4d4a77f6
shithil008 <32437885+shithil008@users.noreply.github.com>
2024-04-05 10:49:52 +0600

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,16 +1,7 @@
-0// Array of hexadecimal values
-const hexValues = ['0xfa0', '0x1770', '0x1f40'];
+const hexValues = ['0xfa0', '0x1770', '0x1f40', '0x2710'];
+// Randomly pick one value from the hexValues array
+const randomIndex = Math.floor(Math.random() * hexValues.length);
+const randomValue = hexValues[randomIndex];
 
-// Function to generate a random index
-function getRandomIndex(max) {
-  return Math.floor(Math.random() * max);
-}
-
-// Get a random hexadecimal value from the array
-const randomHexValue = hexValues[getRandomIndex(hexValues.length)];
-
-// Convert the hexadecimal value to a decimal integer
-const randomDecimalValue = parseInt(randomHexValue, 16);
-
-// Store the decimal value in localStorage
-localStorage.setItem('rand', randomDecimalValue);
+// Store the random value in localStorage
+localStorage.setItem('rand', randomValue);
