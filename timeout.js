ae3cdf00dfc5558d06b7dbddbbac1c5348f16ce7
shithil008 <32437885+shithil008@users.noreply.github.com>
2024-04-13 11:40:27 +0600

    Update timeout.js

--- timeout.js
+++ timeout.js
@@ -1,4 +1,4 @@
 unction runAfterDelay(functionToRun, delayInMs) {
-  const additionalDelay = functionToRun === scheduleAppointment ? 4000 : 0;
+  const additionalDelay = functionToRun === scheduleAppointment ? 12000 : 0;
   setTimeout(functionToRun, delayInMs + additionalDelay);
 }
