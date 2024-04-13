0e9ef242e5780b07b8dde0cc5229e468689c0d46
shithil008 <32437885+shithil008@users.noreply.github.com>
2024-04-13 11:49:39 +0600

    Update timeout.js

--- timeout.js
+++ timeout.js
@@ -1,4 +1,15 @@
-unction runAfterDelay(functionToRun, delayInMs) {
-  const additionalDelay = functionToRun === scheduleAppointment ? 12000 : 0;
-  setTimeout(functionToRun, delayInMs + additionalDelay);
+// Function to execute a function after a specified delay
+function runAfterDelay(func, delay) {
+  setTimeout(func, delay);
 }
+
+// Function to schedule an appointment with an additional delay
+function scheduleAppointment(func, initialDelay, additionalDelay) {
+  setTimeout(() => {
+    func();
+    setTimeout(func, additionalDelay);
+  }, initialDelay);
+}
+
+// Export the functions
+export { runAfterDelay, scheduleAppointment };
